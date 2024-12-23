7e0dabe77f4258dfb67d7c23943b6c24fc07ec50
dinildamsith <dinildamsith52@gmail.com>
2024-12-23 17:07:02 +0530

    guides page and guides details card create done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./views/home";
 import SignUpPage from "./views/signUp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SignInPage from "./views/signIn";
+import Guides from "./views/guides";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
                   <Route path="/" element={<Home/>} />
                   <Route path="/sign-up" element={<SignUpPage />} />
                   <Route path="/sign-in" element={<SignInPage />} />
+                  <Route path="/guides" element={<Guides/>} />
               </Routes>
           </BrowserRouter>
       </>
