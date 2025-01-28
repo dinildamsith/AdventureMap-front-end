e1b939163293db2dbb561444220cf9c43f756bac
dinildamsith <dinildamsith52@gmail.com>
2025-01-28 18:25:36 +0530

    changes apply

--- src/component/vehiclesDetailsCard/index.tsx
+++ src/component/vehiclesDetailsCard/index.tsx
@@ -106,7 +106,7 @@ export default function VehicleDetailsCard(props: any) {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">Rs:{props.price}.00</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}.00</span>
                     <button
                         onClick={vehicleImageClick}
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
